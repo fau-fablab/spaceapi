5574b77957335cba0c316db6bbb94714a9beaeeb
Max Gaukler <development@maxgaukler.de>
2018-05-31 14:20:42 +0200

    add option for inverting switch logic

--- misc/update-status.sh
+++ misc/update-status.sh
@@ -5,6 +5,10 @@ cd "$(dirname "${0}")"
 
 KEY="$HOME/door.key"
 DOORSTATE_USER="tuerstatus"
+# invert logic?
+# false: switch closed = door closed
+# true: switch open = door closed 
+DOORSTATE_INVERTED="true"
 
 if [ "${USER}" != "${DOORSTATE_USER}" ]; then
     echo "[!] Please run this script as user ${DOORSTATE_USER}" 1>&2
@@ -16,32 +20,40 @@ function update_status() {
         touch /mnt/ramdisk/tuerstatus.success
 }
 
-# Schauen ob die Türe offen ist
-# Türe offen ==  Schalter von RX nach TX an ttyS0 geschlossen
-# Ausgabe: Zahl (1=offen, 0=zu)
-# Rückgabewert: 1 = Fehler, 0 = okay
+# is door open?
+# echo "1" if open, "0" otherwise
+# return 1 on error
 function is_open() {
 	{
+        if $DOORSTATE_INVERTED; then
+            RETURN_SWITCH_OPEN="0"
+            RETURN_SWITCH_CLOSED="1"
+        else
+            RETURN_SWITCH_OPEN="1"
+            RETURN_SWITCH_CLOSED="0"
+        fi
+        RETURN_ERROR="0"
 		sleep 1
-		# sende "MOEP", lese Antwort
-		# read gibt den Prompt auf stderr aus, deshalb weiter unten die Umleitung von stderr und nicht von stdout
-		read -t 1 -n4 -p "MOEP" ans || { echo "1"; return 0; }
+		# send "MOEP", check response
+		# read returns its prompt on stderr, therefore the redirect below.
+		read -t 1 -n4 -p "MOEP" ans || { echo $RETURN_SWITCH_OPEN; return 0; }
 		if [ "a$ans" == "aMOEP" ]; then
-			# Antwort war MOEP -> Schalter  RX-TX  geschlossen
-			echo "0"; return 0
+			# response is MOEP -- switch is closed
+			echo $RETURN_SWITCH_CLOSED
+			return 0
 		fi;
-		#echo "Fehler beim Auslesen: Antwort war $ans" >&2;
-		echo "0"
+		# error
+		echo $RETURN_ERROR
 		return 1;
 	} < /dev/ttyS0 2> /dev/ttyS0
 }
 
-# zehnmal auslesen, bei >7 mal keine Antwort gilt es als offen
-
+# retry ten times
 n=0
 for i in `seq 0 9`; do
 	n=$(($n + `is_open`))
 done
+#  check if more than 7 times out of 10 the result is "opened"
 if [ $n -gt 7 ]; then
 	update_status "opened"
 else
