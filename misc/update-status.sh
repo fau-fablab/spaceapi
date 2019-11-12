bcb3dcd2ddb2a573862e1e8141fc146d1d331ed2
Chris Spangler <chris.spangler@fau.de>
2019-11-12 10:51:05 +0100

    Add GPIO pin config

--- misc/update-status.sh
+++ misc/update-status.sh
@@ -3,14 +3,30 @@ set +e
 
 cd "$(dirname "${0}")"
 
+###
+# configuration
+###
+
 KEY="$HOME/door.key"
 
 DOORSTATE_USER="tuerstatus"
+
+# GPIO input pin that reads from the sensor
+IN_GPIO_PIN="2"
 
 # invert logic?
 # false: switch closed = door closed
 # true: switch open = door closed 
 DOORSTATE_INVERTED="true"
+
+# GPIO output pin. Set to 0 to disable (for example, if sensor is connected to 3V3)
+OUT_GPIO_PIN="27"
+# What value should be set on the output pin (ignored if $OUT_GPIO_PIN is 0)
+OUT_GPIO_STATE="0"
+
+###
+# script
+###
 
 if [ "${USER}" != "${DOORSTATE_USER}" ]; then
 	echo "[!] Please run this script as user ${DOORSTATE_USER}" 1>&2
@@ -18,8 +34,8 @@ if [ "${USER}" != "${DOORSTATE_USER}" ]; then
 fi
 
 if $DOORSTATE_INVERTED; then
-        RETURN_SWITCH_OPEN="0"
-        RETURN_SWITCH_CLOSED="1"
+	RETURN_SWITCH_OPEN="0"
+	RETURN_SWITCH_CLOSED="1"
 else
 	RETURN_SWITCH_OPEN="1"
 	RETURN_SWITCH_CLOSED="0"
@@ -27,8 +43,8 @@ fi
 RETURN_ERROR="0"
 
 function update_status() {
-        ../spaceapi/doorstate_client.py update --key "${KEY}" --state "${1}"
-        touch /mnt/ramdisk/tuerstatus.success
+	../spaceapi/doorstate_client.py update --key "${KEY}" --state "${1}"
+	touch /mnt/ramdisk/tuerstatus.success
 }
 
 # is door open?
@@ -37,7 +53,7 @@ function update_status() {
 function is_open() {
 	sleep 1
 
-	if [ $(cat /sys/class/gpio/gpio2/value) == 0 ]; then
+	if [ $(cat /sys/class/gpio/gpio$IN_GPIO_PIN/value) == 0 ]; then
 		echo "${RETURN_SWITCH_CLOSED}"
 		return 0
 	else
